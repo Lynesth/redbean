eecd181e2fb585c2681b7e1e61ab7f481b6a6d9c
Lynesth <lyn@nautile.nc>
2018-08-20 02:15:54 +1100

    Minor fix Logger/RDefault/Debug

--- RedBeanPHP/Logger/RDefault/Debug.php
+++ RedBeanPHP/Logger/RDefault/Debug.php
@@ -202,7 +202,7 @@ class Debug extends RDefault implements Logger
 		$sql = func_get_arg( 0 );
 
 		if ( func_num_args() < 2) {
-			$bindings = array();
+			$bindings = NULL;
 		} else {
 			$bindings = func_get_arg( 1 );
 		}
